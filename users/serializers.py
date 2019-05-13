842bdfd8447222e6216a954de044f709631495ce
Ahmed Samy <aahmedsamy.as@gmail.com>
2019-05-13 23:41:10 +0200

    remove phone field from UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email',
-                  'phone', 'searcher', 'publisher')
+                  'searcher', 'publisher')
         depth = 1
 
 
